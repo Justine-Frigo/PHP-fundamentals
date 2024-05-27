572755727e14306f5a7772756d06d46e4918b1f8
Justine-Frigo <justine0912@gmail.com>
2024-05-27 16:33:01 +0200

    add 2-2 et 2-3

---
+++ 2-2-drill-ternary/ternary.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form method="get" action="">
+<label>What is your gender?</label>
+<input type="radio" name="gender" id="female" value="female"/>
+<label for="female">F</label>
+<input type="radio" name="gender" id="male" value="male"/>
+<label for="male">M</label>
+<input type="submit" value="Submit">
+</form>
+
+<?php
+$gender = $_GET['gender'];
+
+$hello = ($gender == "female") ? "Hello Miss" : "Hello Sir";
+echo $hello;
+?>
+</body>
+</html>
---
+++ 2-3-drill-unicorn/unicorn.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="" method="get">
+    <label for="choice">Are you a Human, a Cat or a Unicorn?</label><br>
+    <input type="radio" name="choice" value="human" required><label for="human">Human</label>
+    <input type="radio" name="choice" value="cat" required><label for="cat">Cat</label>
+    <input type="radio" name="choice" value="unicorn" required><label for="unicorn">Unicorn</label><br>
+    <input type="submit" name="submit" value="Submit">
+</form>
+
+<?php
+$human = '<img src="https://media.giphy.com/media/PZSKakN081R83ah0IH/giphy.gif?cid=790b76118btkvh3k2lao1s64sg9obbs4rqe0u39x2be9uc81&ep=v1_gifs_search&rid=giphy.gif&ct=g">';
+$cat = '<img src="https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif?cid=790b7611j7wt4wbcpvq99pkc59qzmbcmu7lexqdic99unxtq&ep=v1_gifs_search&rid=giphy.gif&ct=g">';
+$unicorn = '<img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmRrbmZ5ZWkyaWdkaDFqcWt5ZG95dmk1aG5uZW11azdpcDRmeHFieiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/j0kQJxo5mzGYb4EvWK/giphy.gif">';
+if (isset($_GET['choice'])) {
+    $choice = ($_GET['choice'] == "cat") ? $cat : (($_GET['choice'] == "unicorn") ? $unicorn : $human);
+    echo $choice;
+}
+?>
+    
+</body>
+</html>
