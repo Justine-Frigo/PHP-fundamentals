b0617ba80acd87435d17a4e694888b20a1f0f971
Justine-Frigo <justine0912@gmail.com>
2024-05-29 15:15:32 +0200

    add ex 2 loops

--- 5-boucles/loops.php
+++ 5-boucles/loops.php
@@ -20,6 +20,50 @@ foreach ($pronouns as $pronoun){
     }
 }
 
+echo '<br><br><hr><br><br>';
+
+$i = 1;
+while ($i <= 120) {
+    echo $i . ' ';
+    $i++;
+}
+
+for ($i = 1; $i <= 120; $i++) {
+    echo $i . ' ';
+}
+
+echo '<br><br><hr><br>';
+
+$becode = [
+    'Adrien B', 'Adrien C', 'Angel', 'Arnaud', 'Caroline', 'Dylan', 'Dorian', 'Dzheylyan', 'Giovanni', 'Iliess', 'Isabelle', 'Jordan', 'Julie', 'Justine', 'Ludovic', 'Lyne', 'Manu', 'Maryam', 'Nataliia', 'Nathanaël', 'Stacy', 'Tom', 'Youris', 'Zahra'
+];
+echo '<br>';
+
+foreach ($becode as $student) {
+    echo $student . '<br>';
+}
+
+echo '<br><br><hr><br>';
+
+$countries = [
+    'select' => 'Select a country',
+    'BE' => 'Belgium',
+    'FR' => 'France',
+    'IT' => 'Italy',
+    'ES' => 'Spain',
+    'DE' => 'Germany',
+    'UK' => 'United Kingdom',
+    'NL' => ' Netherlands',
+    'LU' => 'Luxemburg',
+    'PT' => 'Portugal',
+];
+
+echo "<select>";
+foreach ($countries as $key => $value) {
+    echo "<option value =".$key.">".$value."</option>";
+}
+echo "</select>";
+
 
 ?>
     
